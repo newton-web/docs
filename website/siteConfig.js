4d576ff1ab70dfedbb6542903b0cdfc9747e811f
tdsprogramming <tannersiciliano@gmail.com>
2020-02-24 19:06:43 -0800

    Keep Intro

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,8 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'GettingStarted', label: 'Docs'},
-    {blog: true, label: 'Blog'}
+    { doc: 'GettingStarted', label: 'Docs' },
+    { blog: true, label: 'Blog' }
   ],
 
   // If you have users set above, you add it here:
